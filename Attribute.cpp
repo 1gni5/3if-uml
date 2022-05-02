bd11b285a315144ee2ad7e900c38f3856c29729e
Phellycie <100373267+Phellycie@users.noreply.github.com>
2022-05-02 17:05:44 +0200

    Create Attribute.cpp

---
+++ Attribute.cpp
@@ -0,0 +1,62 @@
+/*************************************************************************
+                           Attribute  
+                             -------------------
+    début                : $02/05/2022$
+    copyright            : (C) $2022$ par $CHAUDRON$
+    e-mail               : $felicie.chaudron@insa-lyon.fr$
+*************************************************************************/
+
+//---------- Réalisation de la classe <Attribute> ------------
+
+//---------------------------------------------------------------- INCLUDE
+
+//-------------------------------------------------------- Include système
+using namespace std;
+#include <iostream>
+#include <string>
+
+//------------------------------------------------------ Include personnel
+#include "Attribute.h"
+
+//------------------------------------------------------------- Constantes
+#define NMAP
+
+//----------------------------------------------------------------- PUBLIC
+
+//----------------------------------------------------- Méthodes publiques
+
+
+string Attribute::getUnit()
+{
+        return unit;
+}
+//------------------------------------------------- Surcharge d'opérateurs
+
+//-------------------------------------------- Constructeurs - destructeur
+
+Attribute::Attribute (long attribute , string monUnit, string maDescription)
+{
+	this->attributeID = attribute;
+    this->unit = monUnit;
+    this->description = maDescription;
+	
+	#ifdef MAP
+		cout << "Appel au constructeur de <Attribute>" << endl;
+	#endif
+} //----- Fin du constructeur Attribute
+
+    
+
+Attribute::~Attribute ( )
+{	
+	#ifdef MAP
+		cout << "Appel au destructeur de <Attribute>" << endl;
+	#endif
+} //----- Fin de ~Attribute
+
+
+//------------------------------------------------------------------ PRIVE
+
+//----------------------------------------------------- Méthodes protégées
+
+
